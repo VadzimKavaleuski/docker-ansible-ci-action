25e0d2f4151fd59f0f1a2bff62bf2c4e5b5a54cc
Vadzim Kavaleuski <Vadzim_Kavaleuski@epam.com>
2019-12-17 15:37:09 +0300

    8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -l
 echo start
-# echo "$1"
+echo "$1"
 # mkdir -p "/github/$1"
 # cp -R /github/workspace/* "/github/$1"
 # cp /github/workspace/.yamllint "/github/$1"
 # cp /github/workspace/.ansible-lint "/github/$1"
 # cd "/github/$1"
+pwd
+cd "./$1"
 pwd
 molecule test -s default
